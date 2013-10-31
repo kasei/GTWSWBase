7ea6f0ad8682616431a321b055c83a10f9260f9a
Gregory Todd Williams <greg@evilfunhouse.com>
2013-10-30 20:23:15 -0700

    Added SPARQL JSON Results parser.

---
+++ GTWSWBase/GTWSPARQLResultsJSONParser.h
@@ -0,0 +1,14 @@
+//
+//  GTWSPARQLResultsJSONParser.h
+//  GTWSWBase
+//
+//  Created by Gregory Williams on 10/30/13.
+//  Copyright (c) 2013 Gregory Todd Williams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GTWSWBase.h"
+
+@interface GTWSPARQLResultsJSONParser : NSObject<GTWSPARQLResultsParser>
+
+@end
---
+++ GTWSWBase/GTWSPARQLResultsJSONParser.m
@@ -0,0 +1,73 @@
+//
+//  GTWSPARQLResultsJSONParser.m
+//  GTWSWBase
+//
+//  Created by Gregory Williams on 10/30/13.
+//  Copyright (c) 2013 Gregory Todd Williams. All rights reserved.
+//
+
+#import "GTWSPARQLResultsJSONParser.h"
+#import "GTWLiteral.h"
+#import "GTWBlank.h"
+#import "GTWIRI.h"
+
+@implementation GTWSPARQLResultsJSONParser
+
+- (NSEnumerator*) parseResultsFromData: (NSData*) data settingVariables: (NSMutableSet*) set {
+    NSError* error  = nil;
+    id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    NSLog(@"JSON data: %@", json);
+    if (error) {
+        NSLog(@"%@", error);
+        return nil;
+    }
+    
+    NSNumber* boolean   = json[@"boolean"];
+    if (boolean) {
+        if ([boolean boolValue]) {
+            NSDictionary* result    = @{@".bool": [GTWLiteral trueLiteral]};
+            return [@[result] objectEnumerator];
+        } else {
+            NSDictionary* result    = @{@".bool": [GTWLiteral falseLiteral]};
+            return [@[result] objectEnumerator];
+        }
+    } else {
+        [set addObjectsFromArray:json[@"head"][@"vars"]];
+        NSDictionary* r     = json[@"results"];
+        NSArray* bindings   = r[@"bindings"];
+        NSMutableArray* results = [NSMutableArray array];
+        for (NSDictionary* resultData in bindings) {
+    //        NSLog(@"RESULT DATA: %@", resultData);
+            NSMutableDictionary* result = [NSMutableDictionary dictionary];
+            NSArray* keys   = [resultData allKeys];
+            for (NSString* key in keys) {
+                NSDictionary* termData  = resultData[key];
+                NSString* type  = termData[@"type"];
+                id<GTWTerm> term    = nil;
+                if ([type isEqual: @"uri"]) {
+                    term    = [[GTWIRI alloc] initWithValue:termData[@"value"]];
+                } else if ([type isEqual: @"literal"] || [type isEqual: @"typed-literal"]) {
+                    if (termData[@"xml:lang"]) {
+                        term    = [[GTWLiteral alloc] initWithString:termData[@"value"] language:termData[@"xml:lang"]];
+                    } else if (termData[@"datatype"]) {
+                        term    = [[GTWLiteral alloc] initWithString:termData[@"value"] datatype:termData[@"datatype"]];
+                    } else {
+                        term    = [[GTWLiteral alloc] initWithValue:termData[@"value"]];
+                    }
+                } else if ([type isEqual: @"bnode"]) {
+                    term    = [[GTWBlank alloc] initWithValue:termData[@"value"]];
+                } else {
+                    NSLog(@"Can't turn JSON object into an RDF Term: %@", termData);
+                }
+                if (!term)
+                    return nil;
+                result[key] = term;
+            }
+            [results addObject:result];
+        }
+        NSLog(@"JSON Parsed object: %@", json);
+        return [results objectEnumerator];
+    }
+}
+
+@end
