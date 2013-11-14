0f1dfcf891803a0f0ad851ed3c1893503ae6b405
Gregory Todd Williams <greg@evilfunhouse.com>
2013-11-13 22:18:01 -0800

    Re-licensed under 3-Clause BSD (and included license text for RegexKitLite).

---
+++ GTWSWBase/IRI.h
@@ -0,0 +1,13 @@
+//
+//  IRI.h
+//  GTWSWBase
+//
+//  Created by Gregory Williams on 11/13/13.
+//  Copyright (c) 2013 Gregory Todd Williams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IRI : NSObject
+
+@end
---
+++ GTWSWBase/IRI.m
@@ -0,0 +1,13 @@
+//
+//  IRI.m
+//  GTWSWBase
+//
+//  Created by Gregory Williams on 11/13/13.
+//  Copyright (c) 2013 Gregory Todd Williams. All rights reserved.
+//
+
+#import "IRI.h"
+
+@implementation IRI
+
+@end
